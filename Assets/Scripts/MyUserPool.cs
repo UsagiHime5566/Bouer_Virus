73fcb8ea04dd7e4b2798de6448ff0c34eb468fb3
nikki <cuapopomama@gmail.com>
2021-03-24 05:53:12 +0800

    整合 kinect 捕捉人物 與現場物件 , 附帶人物追蹤

--- Assets/Scripts/MyUserPool.cs
+++ Assets/Scripts/MyUserPool.cs
@@ -10,6 +10,8 @@ public class MyUserPool : HimeLib.SingletonMono<MyUserPool>
     public Button BTN_CreateTextUser;
     public float effectRange = 5;
     public float particleRateNormal = 20;
+    public float UserAttachPointThreshold = 0.7f;
+    public int countDeadToKill = 10;
 
     public UserPoint UP_Prefab;
     public CharacterParticle PT_Text_Prefab;
@@ -19,6 +21,10 @@ public class MyUserPool : HimeLib.SingletonMono<MyUserPool>
     [Header("Test Part")]
     public float randX = 10;
     public float randY = 5;
+
+
+    int uid = 0;
+
     void Start()
     {
         BTN_CreateTextUser.onClick.AddListener(CreateUser);
@@ -33,14 +39,36 @@ public class MyUserPool : HimeLib.SingletonMono<MyUserPool>
 
     public void CreateUser(Vector2 pos){
         var temp = Instantiate(UP_Prefab, new Vector3(pos.x, 0, pos.y), Quaternion.identity, transform);
+        temp.name = $"User - {uid}";
+        uid++;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+        List<Vector3> dectect = new List<Vector3>(PositionManager.instance.UnityDetectResult);
         foreach (Transform item in transform)
         {
-            
+            bool updatePoint = false;
+            for (int i = 0; i < dectect.Count; i++)
+            {
+                float distance = Vector3.Distance(item.position, dectect[i]);
+                if(distance < UserAttachPointThreshold){
+                    item.position = dectect[i];
+                    dectect.RemoveAt(i);
+                    i--;
+                    updatePoint = true;
+                    item.GetComponent<UserPoint>().countDead = 0;
+                    break;
+                }
+            }
+
+            if(!updatePoint){
+                item.GetComponent<UserPoint>().countDead++;
+            }
+        }
+        // if point not be used, create user for point
+        foreach (var item in dectect)
+        {
+            CreateUser(new Vector2(item.x, item.z));
         }
     }
 }
--- Assets/Scripts/UserPoint.cs
+++ Assets/Scripts/UserPoint.cs
@@ -6,17 +6,47 @@ using Onoty3D.CharacterParticleAsset.Scripts;
 public class UserPoint : MonoBehaviour
 {
     public Dictionary<Transform, CharacterParticle> particles;
-    void Start()
+    public int countDead = 0;
+
+    void Awake()
     {
         particles = new Dictionary<Transform, CharacterParticle>();
     }
 
-    // Update is called once per frame
+    void Start(){
+        StartCoroutine(CheckTextAlive());
+    }
+
     void Update()
     {
         CheckUserInRange();
+        if(countDead >= MyUserPool.instance.countDeadToKill)
+            Destroy(gameObject);
     }
 
+    public void DeleteTextWithKey(Transform key){
+        if(particles == null)
+            return;
+
+        if(particles.ContainsKey(key)){
+            Destroy(particles[key].gameObject);
+            particles.Remove(key);
+        }
+    }
+
+    WaitForSeconds wait = new WaitForSeconds(0.5f);
+    IEnumerator CheckTextAlive(){
+        while(true){
+            foreach (var txt in particles)
+            {
+                if(txt.Key == null){
+                    var emit = txt.Value.GetComponent<ParticleSystem>().emission;
+                    emit.enabled = false;
+                }
+            }
+            yield return wait;
+        }
+    }
 
     void CheckUserInRange(){
         foreach (Transform item in MyUserPool.instance.transform)
