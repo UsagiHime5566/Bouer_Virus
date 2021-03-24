595fc00b9a45aaf6aaff8d1a0fa0e4fd04008c77
nikki <cuapopomama@gmail.com>
2021-03-24 12:34:45 +0800

    開啟雙顯示器

---
+++ Assets/Scripts/AllowMultiDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllowMultiDisplay : MonoBehaviour
+{
+    void Start()
+    {
+        ActiveMultiScreen();
+    }
+
+    void ActiveMultiScreen(){
+        Debug.Log("displays connected: " + Display.displays.Length);
+        for (int i = 0; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }
+    }
+}
