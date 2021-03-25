1f01fd536f4f4448099a09a2e6a205341cb69598
nikki <a5321bbq@yahoo.com.tw>
2021-03-25 22:32:25 +0800

    新增碰撞音效

---
+++ Assets/Scripts/CollisionParticleSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionParticleSound : MonoBehaviour
+{
+    public int affectCount = 4;
+    int cur = 0;
+
+    void OnParticleCollision(GameObject other) {
+        cur++;
+        if(cur > affectCount){
+            SoundTest.instance.PlayShot();
+            cur = 0;
+            Debug.Log("Curr");
+        }
+    }
+}
---
+++ Assets/Scripts/SoundTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTest : HimeLib.SingletonMono<SoundTest>
+{
+    public List<AudioSource> audioSource;
+    public float pinch = 2;
+    public float vol = 1;
+
+    void Start(){
+        audioSource = new List<AudioSource>(GetComponents<AudioSource>());
+        foreach (var item in audioSource)
+        {
+            item.pitch = pinch;
+            item.volume = vol;
+        }
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            //audioSource.PlayOneShot(audioSource.clip);
+        }
+    }
+
+    public void PlayShot(){
+        int rand = Random.Range(0, audioSource.Count);
+        audioSource[rand].PlayOneShot(audioSource[rand].clip); 
+    }
+}
